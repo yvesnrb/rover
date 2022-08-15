125dff038d42d07e617fe8fca7567dea15a0c858
Yves Bode <ynrbode@gmail.com>
2022-08-15 09:21:14 -0300

    docs: document the serializer provider
    The 'serialize' method was missing a doc string.

--- src/providers/serializer.provider.ts
+++ src/providers/serializer.provider.ts
@@ -5,5 +5,11 @@ import { Position } from "@entities/position.entity";
  * project's spec.
  */
 export interface SerializerProvider {
+  /**
+   * Serialize final rover positions.
+   *
+   * @param positions - An array of positions to serialize.
+   * @returns Serialized string.
+   */
   serialize(positions: Position[]): string;
 }
